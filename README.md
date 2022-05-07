d9d8eabefe85b2cce90d5b58e4d5c6ff82fdd4dd
Paul <paulhaeussler@gmx.de>
2022-05-07 23:44:44 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sadpanda Extension
+
+This is an extension for chrome to to take you to a random sadpanda gallery with the press of a button.
+
+### How to Install:
+
+- Download and move the app folder to somewhere
+- Go to Chrome - More Tools - Extensions or to chrome://extensions
+- Click on Load unpacked extension and provide the path where you downloaded it
+- If it doesnt show up in your extension bar, click on the puzzle piece, locate the extension in the list and pin it
+
+
+Please note: You can only roll a random site if youre already on sadpanda, if you click the button on some other page (google or smt), it won't work
---
+++ app/randomURL.js
@@ -0,0 +1,63 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+// Set 946 to your max page count on the front page
+var pid = getRandomIntInclusive(0, 946)
+console.log(pid)
+fetch("https://e-hentai.org/?page=" + pid, {headers: [
+        ['charset', 'utf-8'],
+    ]})
+    .then(response => response.text())
+    .then((response) => {
+        var links = null
+        if (response.includes("<table class=\"itg gltm\">")){
+            //Minimal
+            console.log("Minimal mode")
+            links = response.split("<td class=\"gl3m glname\"")
+            links.splice(0, 1)
+            var cl = []
+            for (const l of links) {
+                var t = l.split("><a href=\"")[1]
+                cl.push(t)
+            }
+            links = cl
+        } else if (response.includes("<table class=\"itg gltc\">")) {
+            //Compact
+            console.log("Compact mode")
+            links = response.split("<td class=\"gl3c glname\"")
+            links.splice(0, 1)
+            var cl = []
+            for (const l of links) {
+                var t = l.split("><a href=\"")[1]
+                cl.push(t)
+            }
+            links = cl
+        } else if (response.includes("<table class=\"itg glte\">")) {
+            //Extended
+            console.log("Extended mode")
+            links = response.split("<td class=\"gl1e\"")
+            links.splice(0, 1)
+            var cl = []
+            for (const l of links) {
+                var t = l.split("><a href=\"")[1]
+                cl.push(t)
+            }
+            links = cl
+        } else if (response.includes("<div class=\"itg gld\">")) {
+            //Thumbnail
+            console.log("Thumbnail mode")
+            links = response.split("<div class=\"gl1t\"><a href=\"")
+            links.splice(0, 1)
+
+        }
+
+        var lid = getRandomIntInclusive(0, links.length - 1)
+        var link = links[lid]
+        var link = link.split("\"")[0]
+        console.log(link)
+        window.open(link, "_blank")
+    }
+)
